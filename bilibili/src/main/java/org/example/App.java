a5e3a23d978e8cd687472cb2f61c69a0f72bb276
srcrs <531238678@qq.com>
2020-06-07 06:32:30 +0800

    修改规则

--- bilibili/src/main/java/org/example/App.java
+++ bilibili/src/main/java/org/example/App.java
@@ -16,16 +16,18 @@ public class App
         chromeOptions.setHeadless(Boolean.TRUE);
         System.setProperty("webdriver.chrome.driver","chromedriver");
 		chromeOptions.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
-        int n=Integer.valueOf(args[0]);
+        int n=3;
         while(n--!=0){
-            WebDriver driver = new ChromeDriver(chromeOptions);
-            Thread.sleep(2000);
-            driver.get(args[1]);
-            Thread.sleep(3000);
-            driver.findElement(By.xpath("//*[@id=\"bilibiliPlayer\"]/div[1]/div[1]/div[9]/video")).click();
-            Thread.sleep(20000);
-            driver.close();
-			Thread.sleep(60000);
+            if(!("000".equals(args[n]))){
+                WebDriver driver = new ChromeDriver(chromeOptions);
+                Thread.sleep(2000);
+                driver.get(args[n]);
+                Thread.sleep(3000);
+                driver.findElement(By.xpath("//*[@id=\"bilibiliPlayer\"]/div[1]/div[1]/div[9]/video")).click();
+                Thread.sleep(60000);
+                driver.close();
+			    Thread.sleep(20000);
+            }
         }
     }
 }
