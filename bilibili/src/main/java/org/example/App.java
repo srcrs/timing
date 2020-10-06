e0b868e226de7e07b4bcbdfd073c77abc5d27c76
srcrs <srcrs@foxmail.com>
2020-10-06 10:57:03 +0800

    增添UA

--- bilibili/src/main/java/org/example/App.java
+++ bilibili/src/main/java/org/example/App.java
@@ -15,6 +15,7 @@ public class App
     public static void main( String[] args ) throws Exception {
         ChromeOptions chromeOptions=new ChromeOptions();
         chromeOptions.setHeadless(Boolean.TRUE);
+		chromeOptions.addArguments("User-Agent=Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36");
         System.setProperty("webdriver.chrome.driver","chromedriver");
 		chromeOptions.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
         Random random = new Random();
