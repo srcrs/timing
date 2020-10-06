65d9e892aa83a8d6438200f31e792d4ea0c13112
srcrs <srcrs@foxmail.com>
2020-10-06 11:10:56 +0800

    增添UA

--- bilibili/src/main/java/org/example/App.java
+++ bilibili/src/main/java/org/example/App.java
@@ -26,7 +26,7 @@ public class App
                 Thread.sleep(2000);
                 driver.get(args[n]);
                 Thread.sleep(3000);
-                driver.findElement(By.xpath("//*[@id="bilibiliPlayer"]/div/div/div[2]/div[2]/div[5]/i")).click();
+                driver.findElement(By.xpath("//*[@id='bilibiliPlayer']/div/div/div[2]/div[2]/div[5]/i")).click();
                 //观看视频时间随机。
                 Thread.sleep((20+random.nextInt(50))*1000);
                 driver.close();
