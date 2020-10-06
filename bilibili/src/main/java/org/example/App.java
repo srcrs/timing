acc5d3a2e3eadb90fae1328eff94af495c9f73dd
srcrs <srcrs@foxmail.com>
2020-10-06 11:22:05 +0800

    增添UA

--- bilibili/src/main/java/org/example/App.java
+++ bilibili/src/main/java/org/example/App.java
@@ -17,21 +17,20 @@ public class App
         chromeOptions.setHeadless(Boolean.TRUE);
 		chromeOptions.addArguments("User-Agent=Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Mobile Safari/537.36");
         System.setProperty("webdriver.chrome.driver","chromedriver");
-		chromeOptions.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+		WebDriver driver = new ChromeDriver(chromeOptions);
         Random random = new Random();
         int n=3;
         while(n--!=0){
             if(!("000".equals(args[n]))){
-                WebDriver driver = new ChromeDriver(chromeOptions);
                 Thread.sleep(2000);
                 driver.get(args[n]);
                 Thread.sleep(3000);
                 driver.findElement(By.xpath("//*[@id='bilibiliPlayer']/div/div/div[2]/div[2]/div[5]/i")).click();
                 //观看视频时间随机。
                 Thread.sleep((20+random.nextInt(50))*1000);
-                driver.close();
 			    Thread.sleep(20000);
             }
         }
+		driver.close();
     }
 }
