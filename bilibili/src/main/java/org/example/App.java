779abb245a09ca5ba0c350b318be8414bf95da1e
srcrs <srcrs@foxmail.com>
2020-10-06 12:48:32 +0800

    增添UA

--- bilibili/src/main/java/org/example/App.java
+++ bilibili/src/main/java/org/example/App.java
@@ -19,22 +19,22 @@ public class App
 		WebDriver driver = new ChromeDriver(chromeOptions);
         Random random = new Random();
         int n=3;
-        try{
-            while(n--!=0){
-                if(!("000".equals(args[n]))){
-                    Thread.sleep(5000);
-                    driver.get(args[n]);
-                    Thread.sleep(5000);
+        while(n--!=0){
+            if(!("000".equals(args[n]))){
+                Thread.sleep(5000);
+                driver.get(args[n]);
+                Thread.sleep(5000);
+                try{
                     driver.findElement(By.xpath("//*[@id=\"bilibiliPlayer\"]/div[1]/div[1]/div[10]/div[2]/div[2]/div[1]/div[1]/button[1]")).click();
-                    //观看视频时间随机。
-                    Thread.sleep((20+random.nextInt(50))*1000);
-                    Thread.sleep(20000);
+                    System.out.println(n+"------------->播放成功");
+                } catch(Exception e){
+                    System.out.println(n+"------------->播放失败\n"+e);
                 }
+                //观看视频时间随机。
+                Thread.sleep((20+random.nextInt(50))*1000);
+                Thread.sleep(20000);
             }
-        } catch (Exception e){
-            System.out.println(e);
-        } finally {
-            driver.close();
         }
+        driver.close();
     }
 }
